49dca4e711d6f918f70de9488868e9a47840c3bc
Rouven Lührs <rouven@luehrs.dev>
2022-04-22 21:27:45 +0200

    added listening logic from express

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const port = 8000;
 
 const { Client } = require("@notionhq/client");
 
@@ -9,6 +10,10 @@ const NOTION_KEY = process.env.NOTION_KEY;
 const NOTION_DATABASE_ID = process.env.NOTION_DATABASE_ID;
 
 const notion = new Client({ auth: NOTION_KEY });
+
+app.listen(port, () => {
+    console.log(`Example app listening on port http://localhost:${port}`)
+})
 
 app.get('/', (req, res) => {
     console.log("hello");

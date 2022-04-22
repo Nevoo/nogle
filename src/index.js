e253cb05a83193f6fbf4ee81087eebe6a27524f5
Rouven Lührs <rouven@luehrs.dev>
2022-04-22 21:20:00 +0200

    added express js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-const http = require("http");
+const express = require("express");
+const app = express();
 
 const { Client } = require("@notionhq/client");
 
@@ -9,9 +10,8 @@ const NOTION_DATABASE_ID = process.env.NOTION_DATABASE_ID;
 
 const notion = new Client({ auth: NOTION_KEY });
 
-console.log('testt');
-
-const server = http.createServer((request, response) => {
-    response.statusCode = 200;
-    response.end("Hello world");
+app.get('/', (req, res) => {
+    res.send("Hello world");
 });
+
+console.log('testt');

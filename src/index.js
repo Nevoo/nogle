6999294fca6993e93eb1d40cbdc3967316144a24
Rouven Lührs <rouven@luehrs.dev>
2022-04-22 21:45:48 +0200

    adjusted express config

--- src/index.js
+++ src/index.js
@@ -1,23 +1,9 @@
-const express = require("express");
-const app = express();
-const port = 8000;
+require('dotenv').config();
 
 const { Client } = require("@notionhq/client");
-
-require('dotenv').config();
+const { server } = require("./server");
 
 const NOTION_KEY = process.env.NOTION_KEY;
 const NOTION_DATABASE_ID = process.env.NOTION_DATABASE_ID;
 
 const notion = new Client({ auth: NOTION_KEY });
-
-app.listen(port, () => {
-    console.log(`Example app listening on port http://localhost:${port}`)
-})
-
-app.get('/', (req, res) => {
-    console.log("hello");
-    res.send("Hello world");
-});
-
-console.log('testt');
---
+++ src/server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const path = require('path');
+const app = express();
+const port = 8080;
+
+app.get('/', (req, res) => {
+    console.log("hello");
+    res.send("Hello world");
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port http://localhost:${port}/`);
+})
+
+console.log('testt');
+
+module.exports = { server: app };

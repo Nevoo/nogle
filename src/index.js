6d0b6e652cb88849b9be1c04bf1a133e04912f65
Rouven Lührs <rouven.luehrs@bytabo.de>
2022-04-22 16:35:01 +0200

    initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+const https = require("https");
+
+const { Client } = require("@notionhq/client");
+
+require('dotenv').config();
+
+const NOTION_KEY = process.env.NOTION_KEY;
+const NOTION_DATABASE_ID = process.env.NOTION_DATABASE_ID;
+
+const notion = new Client({ auth: NOTION_KEY });

6435ea981c3280f27d115a1dbfc9f5fd2d7f569e
Rouven Lührs <rouven@luehrs.dev>
2022-04-25 16:28:17 +0200

    implemented google OAuth2

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 require('dotenv').config();
 
 const { Client } = require("@notionhq/client");
-const { server } = require("./server");
+const { app } = require("./server");
+const bodyParser = require("body-parser");
+
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 const NOTION_KEY = process.env.NOTION_KEY;
 const NOTION_DATABASE_ID = process.env.NOTION_DATABASE_ID;
--- src/server.js
+++ src/server.js
@@ -1,17 +1,47 @@
 const express = require("express");
+const {google} = require('googleapis');
+
+
 const path = require('path');
 const app = express();
 const port = process.env.PORT || 8080;
 
-app.get('/', (req, res) => {
-    console.log("hello");
-    res.send("Hello world");
+app.get('/', async (req, res) => {
+    const googleAuthConfig = {
+        clientId: process.env.GOOG_CLIENT_ID,
+        clientSecret: process.env.GOOG_CLIENT_SECRET,
+        redirect: process.env.GOOG_REDIRECT,
+    };
+
+    const auth = new google.auth.OAuth2(
+        googleAuthConfig.clientId, 
+        googleAuthConfig.clientSecret, 
+        googleAuthConfig.redirect,
+    );
+
+    const defaultScope = [
+        'https://www.googleapis.com/auth/calendar.events.readonly',
+    ];
+
+    const url = auth.generateAuthUrl({
+        access_type: 'offline',
+        prompt: 'consent', // access type and approval prompt will force a new refresh token to be made each time
+        scope: defaultScope
+    });
+
+    res.redirect(url);
 });
+
+app.get('/dashboard', (req, res) => {
+    console.log(req.query.code);
+    console.log(req.query.scope);
+
+    res.send('logged in');
+})
 
 app.listen(port, () => {
     console.log(`Example app listening on port http://localhost:${port}/`);
 })
 
-console.log('testt');
 
-module.exports = { server: app };
+module.exports = { app };
